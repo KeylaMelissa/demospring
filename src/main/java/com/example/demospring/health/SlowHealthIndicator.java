e787737b9045119eb81bcdc4722226ea20d61ba6
KeylaMelissa <keylatoscano99@gmail.com>
2026-05-14 15:31:41 -0500

    5s liveness

--- src/main/java/com/example/demospring/health/SlowHealthIndicator.java
+++ src/main/java/com/example/demospring/health/SlowHealthIndicator.java
@@ -5,7 +5,7 @@ import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 
-@Component
+@Component("slow")
 public class SlowHealthIndicator implements HealthIndicator {
 
     @Override
