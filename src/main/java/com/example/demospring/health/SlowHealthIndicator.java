d59e6ff5f744b514f5980bddc746eab12179f0e8
KeylaMelissa <keylatoscano99@gmail.com>
2026-05-13 23:08:19 -0500

    add slow endpoints

---
+++ src/main/java/com/example/demospring/health/SlowHealthIndicator.java
@@ -0,0 +1,23 @@
+package com.example.demospring.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SlowHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+
+        try {
+            Thread.sleep(70000); // 70 segundos
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        return Health.up()
+                .withDetail("delay", "70 segundos")
+                .build();
+    }
+}
